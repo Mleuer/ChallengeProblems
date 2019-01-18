6c451dcf440c70e60498f98a2074a6f5def5566c
Adam J. Leuer <aleuer2@illinois.edu>
2019-01-17 22:02:54 -0600

    Fixed method name

--- ChallengeProblems/CharDistributionCalculator.cs
+++ ChallengeProblems/CharDistributionCalculator.cs
@@ -12,17 +12,17 @@ namespace ChallengeProblems
 		///
 		/// The return value is a Dictionary (i.e. a hashmap) where the keys are each character that appeared in the string,
 		/// and the values are the percentage of the string made up by each character. For example, for the string "here",
-		/// computeCharacterDistribution() would return a Dictionary with the values { {'e', 0.5}, {'h', 0.25}, {'r', 0.25} }
+		/// ComputeCharacterDistribution() would return a Dictionary with the values { {'e', 0.5}, {'h', 0.25}, {'r', 0.25} }
 		///
-		/// computeCharacterDistribution() is not case sensitive. In other words, it treats 'A' the same as 'a', and the string
+		/// ComputeCharacterDistribution() is not case sensitive. In other words, it treats 'A' the same as 'a', and the string
 		/// "Eel" would be computed as being made up of 0.67 'e' and 0.33 'l'. All characters in the Dictionary it returns will be
 		/// lower case.
 		/// </summary>
 		/// <returns>A Dictionary (i.e. a hashmap) where the keys are each character that appeared in the string,
 		/// and the values are the percentage of the string made up by each character.</returns>
-		public Dictionary<char, float> computeCharacterDistribution(String text)
+		public Dictionary<char, float> ComputeCharacterDistribution(String text)
 		{
-			throw new Exception("computeCharacterDistribution() doesn't do anything yet!");
+			throw new Exception("ComputeCharacterDistribution() doesn't do anything yet!");
 		}
 		
 	}
